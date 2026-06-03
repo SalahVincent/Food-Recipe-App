818e633da6b06c20675f13f49bc547e3141971f9
SalahVincent <vincentsalah3@gmail.com>
2026-06-03 11:20:45 +0100

    name comment to test deployment

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <a href='https://www.rebasecodecamp.com/' className='px-10 my-10 flex justify-center text-gray-400'>Rebase Code Camp</a>
+    // Vincent Salah
   )
 }
 
